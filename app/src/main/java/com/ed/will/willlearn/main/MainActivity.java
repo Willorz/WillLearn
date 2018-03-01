dbf6189530bbf973cd2b816af1f7ba33438f0438
Willorz <onlyzjt@163.com>
2018-03-01 14:14:31 +0800

    测试 透明标题

--- app/src/main/java/com/ed/will/willlearn/main/MainActivity.java
+++ app/src/main/java/com/ed/will/willlearn/main/MainActivity.java
@@ -2,6 +2,9 @@ package com.ed.will.willlearn.main;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -30,6 +33,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void initView() {
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setNavigationBarColor(Color.TRANSPARENT);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
         recyclervcardvUse=findViewById(R.id.btn_rcvcdv);
     }
 
